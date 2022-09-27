0cee2a8a11265ec213bc49f662b44d0ff468ee09
leo <leo493852107@163.com>
2022-09-27 13:33:44 +0800

    使用JacksonObjectMapper，扩展mvc框架的消息转换器，将Long类型转换为String类型，避免前端拿到id失真，并优化时间格式。启用、禁用员工账号

--- src/main/java/com/leo23/config/WebMvcConfig.java
+++ src/main/java/com/leo23/config/WebMvcConfig.java
@@ -1,8 +1,13 @@
 package com.leo23.config;
 
+import com.leo23.common.JacksonObjectMapper;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
@@ -13,5 +18,20 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
          */
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+
+    /**
+     * 扩展mvc框架的消息转换器
+     *
+     * @param converters
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // 创建消息转换器对象
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        // 设置对象转换器，底层使用Jackson将Java对象转换为json
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+        // 将上面的消息转换器对象追加到mvc框架的转换器集合中，放在最前面(0)
+        converters.add(0, messageConverter);
     }
 }
